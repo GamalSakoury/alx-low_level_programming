b8652ca44648f1bc98c5d4203ddb84d26e17dfab
GamalSakoury <alxgamalsakoury@gmail.com>
2023-03-17 17:31:08 -0700

    c printing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
  * main function that prints a string to the stream output
- * return = 0 (success)
+ * Return: 0 (success)
  */
-int main(void) 
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
