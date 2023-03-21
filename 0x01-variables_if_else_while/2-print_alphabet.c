9a6374b8c866c3e81b390c1394ed5784ae45eb97
GamalSakoury <alxgamalsakoury@gmail.com>
2023-03-20 17:54:32 -0700

    printing the alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A programming declaring a variable called n
+ * to print the alphabets in lowercase
+ * by using a for loop and two putchar
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97;n <= 122; n++)
+	{
+		putchar(n);
+		putchar('\n');
+	}
+	return (0);
+}
