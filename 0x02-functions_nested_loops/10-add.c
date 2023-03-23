f014788d95cef84d2362773b0e804c6b9657367d
GamalSakoury <alxgamalsakoury@gmail.com>
2023-03-23 12:54:51 -0700

    problem 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * add -  a function that adds two numbers.
+  * @n: first parameter.
+  * @m: second parameter.
+  * var1 + var2.
+  * Return: reults of adding.
+  */
+
+int add(int n, int m)
+{
+	return (n + m);
+}
