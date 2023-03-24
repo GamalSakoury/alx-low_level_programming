8feaaed58e1961bd25044f065e8162e78235702e
GamalSakoury <alxgamalsakoury@gmail.com>
2023-03-23 17:42:27 -0700

    problem 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - function that prints a string to the stream output
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
