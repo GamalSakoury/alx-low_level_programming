ac1b30efa95d6b7b35fbf482b3fe922be10c3efd
GamalSakoury <alxgamalsakoury@gmail.com>
2023-03-23 19:37:35 -0700

    8. You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_line - A function that draws a straight line in the terminal.
+ * @size: parameter that is passed to the function size of the sqaures.
+ * using nested for loops.
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
