ccbe16ccf74595cef8b594cce843ebeb10e19694
GamalSakoury <alxgamalsakoury@gmail.com>
2023-03-31 04:18:20 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main -Check the code.
+  *
+  * Return: 0 always.
+  */
+
+int main(void)
+{
+	int i, n;
+	int t1, t2;
+	int nextTerm;
+
+	t1 = 1;
+	t2 = 2;
+	nextTerm = t1 + t2;
+	printf("%d, %d", t1, t2);
+	for (i = 3; i <= 10; ++i)
+	{
+		printf(", %d", nextTerm);
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+	}
+
+	return (0);
+}
